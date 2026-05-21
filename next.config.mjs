0f2322b43a83c2888e170700672ea610b7351a31
colindecorce <decorcecolin@gmail.com>
2026-05-21 20:39:55 +0100

    fixing CORS on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://sotw-back-akdj.onrender.com/:path*",
+      },
+    ];
+  },
 }
 
 export default nextConfig
